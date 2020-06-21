db3659dc62e97d4ec6feef742194d3b515ad1f62
ApoorvSadana <apoorvsadana@users.noreply.github.com>
2020-06-21 22:37:33 +0530

    small changes

--- phone_call_updated.py
+++ phone_call_updated.py
@@ -8,9 +8,9 @@ auth_token = 'fa111419a6f4998c2b8ff33b23b6c083'
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
-                        url='http://demo.twilio.com/docs/voice.xml',
-                        to='+919818394095',
-                        from_='+19282603727'
-                    )
+    url='http://demo.twilio.com/docs/voice.xml',
+    to='+919381872407',  # CHECK IF ITS ON TWILIO
+    from_='+19282603727'
+)
 
 print(call.sid)
